d6a3652b71ec181549b0dba0dac2a73800cb799f
Sergio MT <sergio.mt88@gmail.com>
2025-09-22 19:35:21 +0200

    add eventListener for adding courses to shopping chart

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const carrito = document.querySelector("#carrito");
+const contenedorCarrito = document.querySelector("#lista-carrito tbody");
+const vaciarCarrito = document.querySelector("#vaciar-carrito");
+const listaCursos = document.querySelector("#lista-cursos");
+
+cargarEventListeners();
+function cargarEventListeners() {
+  listaCursos.addEventListener("click", agregarCurso);
+}
+
+function agregarCurso(e) {
+  e.preventDefault();
+  if (e.target.classList.contains("agregar-carrito")) {
+    console.log(e.target);
+  }
+}
