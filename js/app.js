bec66206e73f08c23e2b3cf2228597944ea68957
Sergio MT <sergio.mt88@gmail.com>
2025-09-23 13:14:14 +0200

    add rows to cart and destructure course object

--- js/app.js
+++ js/app.js
@@ -28,7 +28,6 @@ function leerDatosCursos(curso) {
   };
 
   articulosCarrito = [...articulosCarrito, infoCurso];
-  console.log(articulosCarrito);
 
   carritoHTML();
 }
@@ -37,10 +36,24 @@ function carritoHTML() {
   limpiarHTML();
 
   articulosCarrito.forEach((curso) => {
+    const { imagen, titulo, precio, cantidad, id } = curso;
     const row = document.createElement("tr");
+
     row.innerHTML = `
       <td>
-        ${curso.titulo}
+        <img src="${imagen}" width="100">
+      </td>
+      <td>
+        ${titulo}
+      </td>
+      <td>
+        ${precio}
+      </td>
+      <td>
+        ${cantidad}
+      </td>
+      <td>
+        <a href="#" class="borrar-curso" data-id="${id}"> X </a>
       </td>
     `;
 
