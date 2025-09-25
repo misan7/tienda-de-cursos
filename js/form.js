1a1a796576dec3fd355c464627c6a42e8447ea05
Sergio MT <sergio.mt88@gmail.com>
2025-09-25 13:06:48 +0200

    Create Contact page with a Form

---
+++ js/form.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const inputEmail = document.querySelector("#email");
+  const inputAsunto = document.querySelector("#asunto");
+  const inputMensaje = document.querySelector("#mensaje");
+});
