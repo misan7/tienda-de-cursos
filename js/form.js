ca46b5bdfec5b370331db2d7beff49f4faddbbd4
Sergio MT <sergio.mt88@gmail.com>
2025-09-26 17:19:38 +0200

    Show alert when error in form

--- js/form.js
+++ js/form.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const inputEmail = document.querySelector("#email");
   const inputAsunto = document.querySelector("#asunto");
   const inputMensaje = document.querySelector("#mensaje");
+  const formulario = document.querySelector("#formulario");
 
   inputEmail.addEventListener("blur", validar);
   inputAsunto.addEventListener("blur", validar);
@@ -9,9 +10,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function validar(e) {
     if (e.target.value.trim() == "") {
-      console.log("está vacío...");
+      mostrarAlerta();
     } else {
       console.log("mensaje", e.target.value);
     }
   }
 });
+
+function mostrarAlerta() {
+  const error = document.createElement("P");
+  error.textContent = "Ha surgido un imprevisto";
+  error.classList.add("bg-red-600", "text-white", "p-2", "text-center");
+
+  formulario.appendChild(error);
+}
