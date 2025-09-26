77e133a443fcfe8e3402c639e759095a82a1fd86
Sergio MT <sergio.mt88@gmail.com>
2025-09-26 17:07:59 +0200

    add trim to input

--- js/form.js
+++ js/form.js
@@ -2,4 +2,16 @@ document.addEventListener("DOMContentLoaded", function () {
   const inputEmail = document.querySelector("#email");
   const inputAsunto = document.querySelector("#asunto");
   const inputMensaje = document.querySelector("#mensaje");
+
+  inputEmail.addEventListener("blur", validar);
+  inputAsunto.addEventListener("blur", validar);
+  inputMensaje.addEventListener("blur", validar);
+
+  function validar(e) {
+    if (e.target.value.trim() == "") {
+      console.log("está vacío...");
+    } else {
+      console.log("mensaje", e.target.value);
+    }
+  }
 });
